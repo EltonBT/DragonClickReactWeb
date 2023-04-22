b313421106caf775514847ed07ce6a186ca70b78
EltonBT <117984601+EltonBT@users.noreply.github.com>
2023-04-22 01:47:00 +0000

    Deploying to gh-pages from @ EltonBT/DragonClickReactWeb@8be941fd1f580a2feb81924fe0ef53b1ba74dc9c 🚀

--- _next/static/dqZvRQsKX4rCswKbnKVKN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/472-401bc21411bbe339.js","static/css/54814340956b1672.css","static/chunks/pages/index-e13b35c578ade8f5.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/pIQD0EYN7dacc7ugt3ESP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/472-025c40e8d4e97a97.js","static/css/54814340956b1672.css","static/chunks/pages/index-f0eb4bc927e6d2ab.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
